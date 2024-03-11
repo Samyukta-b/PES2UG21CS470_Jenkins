b811e15291f0325fa14cacdc78f8dc22ca7f3d02
Samyukta <samyuktab22@gmail.com>
2024-03-11 22:35:33 +0530

    More coffee

--- main/hello.cpp
+++ main/hello.cpp
@@ -6,5 +6,6 @@ int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "This is CC lab.\n";
+  cout << "I need coffee to complete this assingment.\n";
   return 0;
 }
