19e964875c0a6a45d4aa4964e4cdba90951b8c15
Samyukta <samyuktab22@gmail.com>
2024-03-11 21:46:26 +0530

    Updating hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -5,6 +5,6 @@ using namespace std;
 int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
-  cout << "I have successfully built and run\n";
+  cout << "This is CC lab.\n";
   return 0;
 }
